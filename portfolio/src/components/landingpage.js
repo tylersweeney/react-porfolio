f1305e6f85fe1f23f473c14c3e3a4dbd62944456
Tyler <tylersweeney@gmail.com>
2018-12-09 15:25:20 -0800

    create cards for projecst in projects component

--- portfolio/src/components/landingpage.js
+++ portfolio/src/components/landingpage.js
@@ -6,7 +6,40 @@ class LandingPage extends Component {
         return(
             <div style={{width: '100%', margin: 'auto'}}>
                 <Grid className='landing-grid'>
-                    <Cell col={12}></Cell>
+                    <Cell col={12}>
+                        <img 
+                        src="https://cdn3.iconfinder.com/data/icons/hipster-trendy-vintage-swag/512/beard2-512.png"
+                        alt="logo"
+                        className="logo"
+                        />
+                        <div className="banner-text">
+                            <h1>Full Stack Web Developer</h1>
+
+                            <hr/>
+                        <p>HTML/CSS | JavaScript | React | NodeJS | Express | MongoDB</p>
+
+                        <div className="social-links">
+                            
+                            {/* Linkedin */}
+                            <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-github-square"  aria-hidden="true" />
+                            </a>
+                            {/* Github */}
+                            <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-linkedin-square"  aria-hidden="true" />
+                            </a>
+                            {/* Freecodecamp */}
+                            <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-free-code-camp"  aria-hidden="true" />
+                            </a>
+                            {/* Youtube */}
+                            <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-youtube-square"  aria-hidden="true" />
+                            </a>
+                        </div>
+
+                        </div>
+                    </Cell>
                 </Grid>
             </div>
         )
--- portfolio/src/components/projects.js
+++ portfolio/src/components/projects.js
@@ -1,10 +1,96 @@
 import React, { Component } from 'react'
+import {Tabs, Tab, Grid, Cell, Card, CardText, CardTitle, CardActions, Button, CardMenu, IconButton} from 'react-mdl'
 
 class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0};
+    }
+
+    toggleCategories() {
+
+        if(this.state.activeTab === 0){
+            return(
+                <div className="projects-grid">
+                    {/* Project 1 */}
+                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                        <CardTitle style={{color: '#fff', height: '176px', background: 
+                        'url(https://cdn.worldvectorlogo.com/logos/react-1.svg) center /cover'}}>React Project #1</CardTitle>
+                        <CardText>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, qui nostrum quo excepturi aperiam eveniet, perspiciatis beatae omnis laudantium, natus dolores tempora cupiditate modi. Modi vitae eaque fugit porro dicta!</CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>CodePen</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+
+                    {/* Project 2 */}
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 
+                    'url(https://cdn.worldvectorlogo.com/logos/react-1.svg) center /cover'}}>React Project #1</CardTitle>
+                    <CardText>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, qui nostrum quo excepturi aperiam eveniet, perspiciatis beatae omnis laudantium, natus dolores tempora cupiditate modi. Modi vitae eaque fugit porro dicta!</CardText>
+                    <CardActions border>
+                        <Button colored>GitHub</Button>
+                        <Button colored>CodePen</Button>
+                        <Button colored>LiveDemo</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                    <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+
+                    {/* // Project 3 */}
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 
+                    'url(https://cdn.worldvectorlogo.com/logos/react-1.svg) center /cover'}}>React Project #1</CardTitle>
+                    <CardText>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, qui nostrum quo excepturi aperiam eveniet, perspiciatis beatae omnis laudantium, natus dolores tempora cupiditate modi. Modi vitae eaque fugit porro dicta!</CardText>
+                    <CardActions border>
+                        <Button colored>GitHub</Button>
+                        <Button colored>CodePen</Button>
+                        <Button colored>LiveDemo</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                    <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+                </div>
+
+            )
+        } else if(this.state.activeTab === 1) {
+            return(
+                <div><h1>This is Angular</h1></div>
+            )
+        } else if(this.state.activeTab === 2) {
+            return(
+                <div><h1>This is VueJS</h1></div>
+            )
+        } else if (this.state.activeTab === 3){
+            return(
+                <div><h1>This is MongoDB</h1></div>
+            )
+        }
+    }
+
+   
+
     render() {
         return(
-            <div>
-                <h1>Projects Page</h1>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId})} ripple>
+                    <Tab>React</Tab>
+                    <Tab>Angular</Tab>
+                    <Tab>VueJS</Tab>
+                    <Tab>MongoDB</Tab>
+                </Tabs>
+
+                    <Grid>
+                        <Cell col={12}>
+                            <div className="content">{this.toggleCategories()}</div>
+                        </Cell>
+                    </Grid>
             </div>
         )
     }
